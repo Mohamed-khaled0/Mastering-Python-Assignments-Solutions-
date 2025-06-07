06d85d5f4a2f67484628fafc88b856b6157c44e3
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2025-06-07 23:58:36 +0300

    Implements the solutions for the first five tasks of the Python course, covering basic variable assignment, printing, and type checking.

---
+++ Assignments 1_To_10.py
@@ -0,0 +1,28 @@
+# 1
+"""
+1-Here is the first task 
+(Multiple Line Comment)
+In this file we will solve all tasks for the python course
+"""
+
+# 2 
+
+myName = 'Mohamed'
+myAge = '24'
+myCountry = 'Egypt'
+
+
+# 3 
+print("Name : " + myName)
+print("Name : " + myAge)
+print("Name : " + myCountry)
+
+print("-----------------------------------------------------------")
+
+# 4 
+print("Hello , My Name Is " + myName + " And I Am " + myAge + " Years Old And Live in " + myCountry  ) 
+
+print("-----------------------------------------------------------")
+
+# 5
+print(type(myName));print(type(myAge));print(type(myCountry))
