183157894c0a1daf99e4b297e6da75e2b42f8562
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2025-06-08 20:44:45 +0300

    Assignments Solution (Lessons 19 to 20 )

---
+++ Assignments 19_To_20.py
@@ -0,0 +1,53 @@
+# 1
+
+print(10)  # int
+print(10.0) # float 
+print(2+5j) # complex
+
+print("-----------------------------------------------------------")
+
+# 2
+x=1+2j
+
+# Print Imaginary Part Here
+# Print Real Part Here
+
+print(f"Imaginary Part is : {x.imag}" )
+print(f"Real Part is : {x.real} " )
+
+print("-----------------------------------------------------------")
+
+# 3
+num = 10
+
+# Needed Ouput
+# 10.0000000000
+print(float(10))
+print("-----------------------------------------------------------")
+
+# 4
+num = 159.650
+
+# Needed Output
+# 159
+# <class 'int'>
+
+print(int(159.650))
+print(type(int(159.650)))
+
+print("-----------------------------------------------------------")
+
+# 5
+
+# 100 ? 115 = -15
+# 50 ? 30 = 1500
+# 21 ? 4 = 1
+# 110 ? 11 = 10
+# 97 ?20 = 4
+
+
+print(100 - 115 )
+print(50 * 30)
+print(21 % 4)
+print(110 // 11)
+print(97 //20)
